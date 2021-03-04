a76418321a9c1c34e8a11e3c4ba7e93339b738e3
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-04 17:50:52 +0700

    fix

--- 6320503114_4_5.c
+++ 6320503114_4_5.c
@@ -2,53 +2,56 @@
 #include<string.h>
 int main()
 {
-    int j,i,n,cup[4]={1,0,0,0},t,t2,k;
+    int j,i,n,cup[4]= {1,0,0,0},t,t2,k;
     scanf("%d",&n);
     char w[n][50];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         scanf("%s",w[i]);
-        for(j=0;j<strlen(w[i]);j++)
+        for(j=0; j<strlen(w[i]); j++)
         {
-            switch(w[i][j])
+            if(w[i][j]=='A')
             {
-                case 'A':
-                    t=cup[2];
-                    cup[2]=cup[1];
-                    cup[1]=t;
-                    break;
-                case 'B':
-                    t=cup[0];
-                    cup[0]=cup[3];
-                    cup[3]=t;
-                    break;
-                case 'C':
-                    t=cup[0];
-                    t2=cup[1];
-                    cup[0]=cup[2];
-                    cup[1]=cup[3];
-                    cup[2]=t;
-                    cup[3]=t2;
-                    break;
-                case 'D':
-                    t=cup[0];
-                    t2=cup[2];
-                    cup[0]=cup[1];
-                    cup[2]=cup[3];
-                    cup[1]=t;
-                    cup[3]=t2;
-                    break;
-                case 'E':
-                    t=cup[0];
-                    t2=cup[1];
-                    cup[0]=cup[3];
-                    cup[1]=cup[2];
-                    cup[3]=t;
-                    cup[2]=t2;
-                    break;
+                t=cup[2];
+                cup[2]=cup[1];
+                cup[1]=t;
+            }
+            else if(w[i][j]=='B')
+            {
+                t=cup[0];
+                cup[0]=cup[3];
+                cup[3]=t;
+
+            }
+            else if(w[i][j]=='C')
+            {
+                t=cup[0];
+                t2=cup[1];
+                cup[0]=cup[2];
+                cup[1]=cup[3];
+                cup[2]=t;
+                cup[3]=t2;
+            }
+            else if(w[i][j]=='D')
+            {
+                t=cup[0];
+                t2=cup[2];
+                cup[0]=cup[1];
+                cup[2]=cup[3];
+                cup[1]=t;
+                cup[3]=t2;
+            }
+            else if(w[i][j]=='E')
+            {
+                t=cup[0];
+                t2=cup[1];
+                cup[0]=cup[3];
+                cup[1]=cup[2];
+                cup[3]=t;
+                cup[2]=t2;
             }
         }
-        for(k=0;k<4;k++)
+        for(k=0; k<4; k++)
         {
             if(cup[k]==1)
                 printf("%d\n",k+1);
