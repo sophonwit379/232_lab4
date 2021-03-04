c7c68f18e703db7f18115b618eb844a7a2c40def
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-04 15:12:12 +0700

    split word

--- 6320503114_4_4.c
+++ 6320503114_4_4.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     int i;
-    char sen[1000];
+    char sen[1000],s[2]=" ",show;
+    char *tmp;
     gets(sen);
+    tmp=strtok(sen,s);
+    while(tmp!=NULL)
+    {
 
+      tmp=strtok(NULL,s);
+    }
 
 
 
