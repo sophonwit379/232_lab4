94040a63649be0978b053e5c728d7aefb51567ca
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-04 14:52:59 +0700

    loop and output

--- 6320503114_4_3.c
+++ 6320503114_4_3.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    char word[200];
+    int i;
+    char word[200],tmp[200];
     scanf("%s",word);
-
+    for(i=0;i<strlen(word);i++)
+    {
+        tmp[i]=word[i+1];
+        if(word[i]!=tmp[i])
+            printf("%c",word[i]);
+    }
 
 
 
