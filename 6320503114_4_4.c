fedf1c04f79368baaeb0d671f4dcde41a96d399f
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-04 15:53:35 +0700

    Complete

--- 6320503114_4_4.c
+++ 6320503114_4_4.c
@@ -1,19 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 int main()
 {
-    int i;
-    char sen[1000],s[2]=" ",show;
+    int i=0;
+    char sen[1000],s[2]=" ";
     char *tmp;
     gets(sen);
     tmp=strtok(sen,s);
     while(tmp!=NULL)
     {
+        if(i==0)
+        {
+            printf("%c",toupper(tmp[0]));
+        }
+        else
+        {
 
-      tmp=strtok(NULL,s);
+            if(strcmp(tmp,"i")!=0&&strcmp(tmp,"of")!=0&&strcmp(tmp,"the")!=0&&strcmp(tmp,"on")!=0&&strcmp(tmp,"at")!=0&&strcmp(tmp,"for")!=0
+                &&strcmp(tmp,"with")!=0&&strcmp(tmp,"a")!=0&&strcmp(tmp,"an")!=0&&strcmp(tmp,"in")!=0&&strcmp(tmp,"and")!=0)
+            {
+                printf("%c",toupper(tmp[0]));
+            }
+        }
+        tmp=strtok(NULL,s);
+        i++;
     }
-
-
 
 
 
