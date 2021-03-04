a0fba91dbf01ee50b4ff1b476dfe3cc44dcfd22e
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-03-04 17:53:54 +0700

    input

---
+++ 6320503114_4_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,max=0,min=1000,x,z;
+    scanf("%d",&a);
+    int A[a][3]={},B[a-2]={};
+    for(b=0;b<a;b++)
+    {
+        scanf("%d %d %d",&A[b][0],&A[b][1],&A[b][2]);
+    }
+
+
+}
